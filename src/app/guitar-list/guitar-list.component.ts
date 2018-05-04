4dd5042d06c4b324e72a3b9f97b67819e9370d38
Matt Miller <matt.f.miller@live.com>
2018-05-04 10:13:52 -0700

    Add content to guitar-list component to display list of guitar.

--- src/app/guitar-list/guitar-list.component.ts
+++ src/app/guitar-list/guitar-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Guitar } from '../models/guitar.model'
 
 @Component({
   selector: 'app-guitar-list',
   templateUrl: './guitar-list.component.html',
   styleUrls: ['./guitar-list.component.css']
 })
-export class GuitarListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class GuitarListComponent {
+  guitarArray: Guitar[] = [
+    new Guitar("G6134T-CDG LIMITED EDITION PENGUIN™ WITH BIGSBY®", "https://www.fmicassets.com/Damroot/GretschVert/10001/2400509846_gtr_frt_001_rr.png"),
+    new Guitar("G6609TFM PLAYERS EDITION BROADKASTER® CENTER BLOCK DOUBLE-CUT WITH STRING-THRU BIGSBY®", "https://www.fmicassets.com/Damroot/GretschVert/10001/2400700878_gtr_frt_001_rr.png"), new Guitar("G6134T-58 VINTAGE SELECT ’58 PENGUIN™ WITH BIGSBY®", "https://www.fmicassets.com/Damroot/GretschVert/10002/2400709805_gtr_frt_001_rr.png")
+  ]
 }
