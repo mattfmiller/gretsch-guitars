680d31391babf7d031cd0e53eea59154f95b83e0
Matt Miller <matt.f.miller@live.com>
2018-05-04 14:14:36 -0700

    Add guitar-filter components to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Guitar } from './models/guitar.model';
+import { GuitarFilter } from './models/guitar-filter.model';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +16,13 @@ export class AppComponent {
     new Guitar("G6609TFM PLAYERS EDITION BROADKASTER® CENTER BLOCK DOUBLE-CUT WITH STRING-THRU BIGSBY®", "https://www.fmicassets.com/Damroot/GretschVert/10001/2400700878_gtr_frt_001_rr.png"), new Guitar("G6134T-58 VINTAGE SELECT ’58 PENGUIN™ WITH BIGSBY®", "https://www.fmicassets.com/Damroot/GretschVert/10002/2400709805_gtr_frt_001_rr.png")
   ]
 
+  masterGuitarFilterArray: GuitarFilter[] = [
+    new GuitarFilter("NEWEST MODELS", "SEE THE LATEST", "../assets/newest.jpg"),
+    new GuitarFilter("THE BIG BODY SOUND", "VIEW HOLLOW BODIES", "../assets/hollow.jpg"),
+    new GuitarFilter("ARTIST MODELS", "VIEW ARTIST SIGNATURES", "../assets/artist.jpg"),
+  ]
+
   runFilter(desiredFilterTerm) {
     this.filterTerm = desiredFilterTerm;
-    console.log(this.filterTerm);
   }
 }
--- src/app/guitar-filter-list/guitar-filter-list.component.ts
+++ src/app/guitar-filter-list/guitar-filter-list.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GuitarFilter } from '../models/guitar-filter.model';
 
 @Component({
   selector: 'app-guitar-filter-list',
   templateUrl: './guitar-filter-list.component.html',
   styleUrls: ['./guitar-filter-list.component.css']
 })
-export class GuitarFilterListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class GuitarFilterListComponent {
+  @Input() childGuitarFilterArray: GuitarFilter [];
 
 }
