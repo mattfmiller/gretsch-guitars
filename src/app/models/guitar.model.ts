cf52a5c7efa55adf1b43922d6d13f7e707d4b321
Matt Miller <matt.f.miller@live.com>
2018-05-09 21:09:47 -0700

    Create alternate guitar model (comment out) and sample-guitar.json with data.

--- src/app/models/guitar.model.ts
+++ src/app/models/guitar.model.ts
@@ -1,3 +1,7 @@
 export class Guitar {
   constructor(public name: string, public image: string, public newness: boolean, public hollow: boolean, public artistModel: boolean) {}
 }
+
+// export class Guitar {
+//   constructor(public name: string, public imageUrl: string, public category: string, public tailpiece: string, public orientation: string, public series: string, public color: string, public modelNumber: number, public description: string) {}
+// }
