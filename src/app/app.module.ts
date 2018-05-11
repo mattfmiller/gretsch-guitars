dca30606959f24d92986d856513c663dfccc1d2c
Matt Miller <matt.f.miller@live.com>
2018-05-10 20:18:21 -0700

    Add welcome component to display guitar filter components and social link components at path ''

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { routing } from './app.routing';
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -24,7 +25,8 @@ export const firebaseConfig = {
     GuitarListComponent,
     GuitarFilterListComponent,
     SocialLinkListComponent,
-    FilterResults
+    FilterResults,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,24 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GuitarFilterListComponent } from './guitar-filter-list/guitar-filter-list.component';
+// import { GuitarFilterListComponent } from './guitar-filter-list/guitar-filter-list.component';
 import { GuitarListComponent } from './guitar-list/guitar-list.component';
-import { SocialLinkListComponent } from './social-link-list/social-link-list.component';
+// import { SocialLinkListComponent } from './social-link-list/social-link-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const appRoutes: Routes = [
+  // {
+  //   path: '',
+  //   component: GuitarFilterListComponent,
+  //   outlet: 'guitarFilterList'
+  // },
+  // {
+  //   path: '',
+  //   component: SocialLinkListComponent,
+  //   outlet: 'socialLinkList'
+  // },
   {
     path: '',
-    component: GuitarFilterListComponent,
-    outlet: 'guitarFilterList'
-  },
-  {
-    path: '',
-    component: SocialLinkListComponent,
-    outlet: 'socialLinkList'
+    component: WelcomeComponent
   },
   {
     path: 'guitars',
