aec70f89a0d4c3c5685117c6b6b86973a02eec2d
Matt Miller <matt.f.miller@live.com>
2018-05-10 22:54:24 -0700

    Use service to provide data from firebase to social-link-list component.

--- src/app/social-link-list/social-link-list.component.ts
+++ src/app/social-link-list/social-link-list.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SocialLink } from '../models/social-link.model';
+import { GuitarService } from '../guitar.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-social-link-list',
   templateUrl: './social-link-list.component.html',
-  styleUrls: ['./social-link-list.component.css']
+  styleUrls: ['./social-link-list.component.css'],
+  providers: [GuitarService]
 })
-export class SocialLinkListComponent {
+export class SocialLinkListComponent implements OnInit{
+  constructor(private guitarService: GuitarService){}
+  socialLinks: FirebaseListObservable<any[]>;
 
-  masterSocialLinkArray: SocialLink[] = [
-    new SocialLink("INSTAGRAM", "../assets/instagram.jpg", "https://www.instagram.com/officialgretsch/"),
-    new SocialLink("FACEBOOK", "../assets/facebook.jpg", "https://www.facebook.com/GretschGuitars"),
-    new SocialLink("YOUTUBE", "../assets/youtube.jpg", "https://www.youtube.com/user/gretschguitars/"),
-    new SocialLink("ARTISTS", "../assets/artists.jpg", "https://gretschguitars.com/artists")
-  ]
+  // masterSocialLinkArray: SocialLink[] = [
+  //   new SocialLink("INSTAGRAM", "../assets/instagram.jpg", "https://www.instagram.com/officialgretsch/"),
+  //   new SocialLink("FACEBOOK", "../assets/facebook.jpg", "https://www.facebook.com/GretschGuitars"),
+  //   new SocialLink("YOUTUBE", "../assets/youtube.jpg", "https://www.youtube.com/user/gretschguitars/"),
+  //   new SocialLink("ARTISTS", "../assets/artists.jpg", "https://gretschguitars.com/artists")
+  // ]
+
+  ngOnInit() {
+    this.socialLinks = this.guitarService.getSocialLinks();
+  }
 }
