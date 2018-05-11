2db7d94ed8d4b9ac692838fbf374b132c4561e56
Matt Miller <matt.f.miller@live.com>
2018-05-10 19:23:36 -0700

    Add route and router-link to guitar-list component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GuitarFilterListComponent } from './guitar-filter-list/guitar-filter-list.component';
+import { GuitarListComponent } from './guitar-list/guitar-list.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: GuitarFilterListComponent
+  },
+  {
+    path: 'guitar-list',
+    component: GuitarListComponent
   }
 ];
 
